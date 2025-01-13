e3f02e28a97ffd422fd458f0d4d746e53e0253b2
NiklasNeighbor <niklaswicker2005@gmail.com>
2025-01-13 16:34:25 +0100

    Created Ingredient class

---
+++ Witchbrew/Assets/Core/Ingrediants/Ingredieant.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredieant : MonoBehaviour
+{
+    public enum ingredient
+    {
+        Herbs,
+        Mushrooms,
+        Eyeballs,
+        Bones,
+        Scales
+    }
+
+
+    public enum preperation
+    {
+        raw,
+        grinded,
+        sliced,
+        roasted
+
+    }
+}
