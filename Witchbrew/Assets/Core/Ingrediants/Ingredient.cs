aab91084ccbc1daf80de83bc29bb9fde9ef2855b
NiklasNeighbor <niklaswicker2005@gmail.com>
2025-01-14 13:50:35 +0100

    correcting misspelling of 'Ingredient'

--- Witchbrew/Assets/Core/Ingrediants/Ingredient.cs
+++ Witchbrew/Assets/Core/Ingrediants/Ingredient.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ingredieant : MonoBehaviour
+public class Ingredient : MonoBehaviour
 {
     public enum ingredient
     {
