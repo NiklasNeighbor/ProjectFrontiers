248be56e652c17f1f8636a0e50529654fd6b250f
NiklasNeighbor <niklaswicker2005@gmail.com>
2025-01-14 15:59:15 +0100

    Started work on checking if a recipe is valid
    DO NOT TOUCH THE POTIONMAKING SCRIPT!

--- Witchbrew/Assets/Core/PotionMaking/scripts/MakePotion.cs
+++ Witchbrew/Assets/Core/PotionMaking/scripts/MakePotion.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [Serializable]
 public class recipe
 {
+    public string RecipeName;
+
     public Ingredient FirstIngredient;
     public Ingredient SecondIngredient;
     public Ingredient ThirdIngredient;
@@ -27,6 +29,30 @@ public class MakePotion : MonoBehaviour
 
     void Update()
     {
+        if(HasIngredients())
+        {
+            CheckRecipes();
+        }
+    }
 
+    public bool HasIngredients()
+    {
+        if (Ingredient1 != null && Ingredient2 != null && Ingredient3 != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void CheckRecipes()
+    {
+        foreach (recipe recipe in potions)
+        {
+            bool HasIngredient1;
+            bool HasIngredient2;
+            bool HasIngredient3;
+
+            //TODO: check recipe to see if it has each ingredient
+        }
     }
 }
