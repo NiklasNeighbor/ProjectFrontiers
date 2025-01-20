78dea8f4a93f39efdcd20f546e849c1f0f795e83
Guus12345 <guuslegters@gmail.com>
2025-01-20 21:17:10 +0100

    Reduced Jitter and fixed floating object glitch
    i reduced the jitter by as much as i could and i fixed the floating object glitch for the Interaction Manager Script

--- Witchbrew/Assets/Imported/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ Witchbrew/Assets/Imported/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -7,35 +7,36 @@ public class FirstPersonLook : MonoBehaviour
     public float sensitivity = 2;
     public float smoothing = 1.5f;
 
-    public float upperClamp;
-    public float lowerClamp;
+    public float upperClamp = 90f; // Default values
+    public float lowerClamp = -90f;
 
     Vector2 velocity;
     Vector2 frameVelocity;
 
-
     void Reset()
     {
-        // Get the character from the FirstPersonMovement in parents.
         character = GetComponentInParent<FirstPersonMovement>().transform;
     }
 
     void Start()
     {
-        // Lock the mouse cursor to the game screen.
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        // Get smooth velocity.
         Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
         frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+
+        // Update the camera rotation
         velocity += frameVelocity;
         velocity.y = Mathf.Clamp(velocity.y, lowerClamp, upperClamp);
+    }
 
-        // Rotate camera up-down and controller left-right from velocity.
+    void LateUpdate()
+    {
+        // Apply rotations after all updates
         transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
         character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
     }
--- Witchbrew/Assets/Imported/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Witchbrew/Assets/Imported/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -3,42 +3,40 @@ using UnityEngine;
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 5f;
 
     [Header("Running")]
     public bool canRun = true;
     public bool IsRunning { get; private set; }
-    public float runSpeed = 9;
+    public float runSpeed = 9f;
     public KeyCode runningKey = KeyCode.LeftShift;
 
-    Rigidbody rigidbody;
-    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    private Vector2 inputDirection;
+    private Rigidbody rigidbody;
+
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
-
-
 
     void Awake()
     {
-        // Get the rigidbody on this.
         rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
     void FixedUpdate()
     {
-        // Update IsRunning from input.
         IsRunning = canRun && Input.GetKey(runningKey);
 
-        // Get targetMovingSpeed.
-        float targetMovingSpeed = IsRunning ? runSpeed : speed;
-        if (speedOverrides.Count > 0)
-        {
-            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
-        }
+        float targetSpeed = speedOverrides.Count > 0
+            ? speedOverrides[speedOverrides.Count - 1]()
+            : (IsRunning ? runSpeed : speed);
 
-        // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        Vector2 targetVelocity = inputDirection * targetSpeed;
+        Vector3 velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
 
-        // Apply movement.
-        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+        rigidbody.velocity = velocity;
     }
 }
